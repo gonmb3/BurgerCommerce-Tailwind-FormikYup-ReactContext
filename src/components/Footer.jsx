1f7bd2ec8acf7f698b75ed6a2c1e6edd11b9aab1
Gonzalo Mos <gon.mb3@gmail.com>
2022-11-25 20:08:56 +0100

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='bg-red-600 py-12'>
       <div className="container mx-auto text-center">
-        <p>
+        <p className="text-[15px]">
           Copyright &copy; Trutru Burgers 2022. All Rights Reserved.
         </p>
       </div>
