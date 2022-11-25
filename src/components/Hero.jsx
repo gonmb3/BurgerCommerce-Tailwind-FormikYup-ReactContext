17f8103f60be564aed7c92767e0b7f526a7bdb7b
Gonzalo Mos <gon.mb3@gmail.com>
2022-11-25 15:10:22 +0100

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,13 @@ import React from 'react'
 const Hero = () => {
   return (
     <section className=' h-[600px] relative  hero-bg '>
+      <div className="absolute w-full top-[55%]">
+        <h2 className='text-center drop-shadow-lg md:text-6xl text-3xl uppercase font-bold text-white'>
+          Welcome to <span className='text-white font-bold '>TRUTRU BURGERS</span>!
+          <p className='text-white text-2xl drop-shadow-lg'>Best Food In Town</p>
+        </h2>
 
-         <div className="absolute w-full top-[55%]">
-                <h2 className='text-center drop-shadow-lg md:text-6xl text-3xl uppercase font-bold text-white'>
-                    Welcome to <span className='text-white font-bold '>TRUTRU BURGERS</span>!
-                    <p className='text-white text-2xl drop-shadow-lg'>Best Food In Town</p>
-                </h2>
-  
-        </div>
-      
-                 
+      </div>
     </section>
   )
 }
